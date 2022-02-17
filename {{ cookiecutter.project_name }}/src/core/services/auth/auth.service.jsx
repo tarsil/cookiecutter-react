136842b4c892ae501f86fb8b1cfaac196938e69d
Tiago A. Silva <tiago.arasilva@outlook.com>
2022-02-17 18:51:03 +0000

    Added JWT integration example

---
+++ {{ cookiecutter.project_name }}/src/core/services/auth/auth.service.jsx
@@ -0,0 +1,38 @@
+import TokenService from "../token.service";
+import api from '../../interceptors/api'
+
+class AuthService {
+
+    login(username, password) {
+      return api
+        .post("/auth/token", {
+          username,
+          password
+        })
+        .then(response => {
+          if (response.data.accessToken) {
+            TokenService.setUser(response.data);
+          }
+          return response.data;
+        });
+    }
+
+    logout() {
+      TokenService.removeUser();
+    }
+
+    register(firstName, lastName, email, password, retypePassword) {
+      return api.post("/accounts/auth/signup", {
+        'first_name': firstName,
+        'last_name': lastName,
+        'email': email,
+        'password': password,
+        'retype_password': retypePassword
+      });
+    }
+    getCurrentUser() {
+      return TokenService.getUser();
+    }
+  }
+
+  export default new AuthService();
