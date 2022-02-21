b0fae84c596e83f55ad1e8169ea7af8a180e2a53
Tiago A. Silva <tiago.arasilva@outlook.com>
2022-02-21 11:35:36 +0000

    Auth component added

---
+++ {{ cookiecutter.project_name }}/src/components/auth/RequireAuth.jsx
@@ -0,0 +1,12 @@
+import { Navigate, useLocation } from 'react-router-dom'
+
+import { useAuth } from '../../core/auth/auth'
+
+function RequireAuth({ children }) {
+  const { authed } = useAuth()
+  const location = useLocation()
+
+  return authed === true ? children : <Navigate to='/login' replace state={{ path: location.pathname }} />
+}
+
+export default RequireAuth
