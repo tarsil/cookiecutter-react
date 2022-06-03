dd3d91d02f682e70cc17e3f7aef42689c8e1047b
Tiago A. Silva <tiago.arasilva@outlook.com>
2022-06-03 07:23:05 +0100

    Fix cookiecutter generator

--- {{ cookiecutter.project_name }}/src/App.jsx
+++ {{ cookiecutter.project_name }}/src/App.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Navigate, Route, Routes } from "react-router-dom";
+import React, { Component } from "react";
+
 import Page from "react-page-loading";
-import { CSSTransition } from "react-transition-group";
-import ScrollUpBtn from './components/shared/ScrollUp';
+import ScrollUpBtn from "./components/shared/ScrollUp";
 
 class App extends Component {
   render() {
@@ -11,14 +11,9 @@ class App extends Component {
         <Page loader={"commet-spin"} color={"#fe5619"} size={50}>
           <Route
             render={({ location }) => (
-              <CSSTransition
-                  key={location.key}
-                  timeout={{ enter: 900, exit: 900 }}
-                  classNames="fade"
-              >
-                  <section className="route-section">
-                      <Routes location={location}>
-                          {/* <Route
+              <section className="route-section">
+                <Routes location={location}>
+                  {/* <Route
                               path="/blog-details"
                               component={SingleBlog}
                           />
@@ -42,16 +37,15 @@ class App extends Component {
                               path="/"
                               component={HomeOne}
                           /> */}
-                          <Navigate to="/not-found" />
-                      </Routes>
-                  </section>
-              </CSSTransition>
+                  <Navigate to="/not-found" />
+                </Routes>
+              </section>
             )}
           />
           <ScrollUpBtn />
         </Page>
       </div>
-    )
+    );
   }
 }
 
